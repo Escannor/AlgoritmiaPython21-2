0f288e9feba6620f31eb510e874c95e115f2190d
Arturo <43208414+Escannor@users.noreply.github.com>
2021-03-02 13:42:38 -0600

    Add files via upload

---
+++ S3/dataTypes.py
@@ -0,0 +1,25 @@
+# Integer
+entero = 15
+print(entero)
+print(type(entero))
+
+#float
+flotante = 56.45
+flotante = 4565.12345
+print(flotante)
+print(type(flotante))
+
+# String
+cadena = "Hola a todos"
+
+# Boolean
+boleano = True
+
+# List
+lista = [1, 2, 3, 4, 5]
+lista = ['a', 'b', 'c', 'd']
+lista = [1, 2, 3, 4, 5, 'a', 'b', 'c', 'd']
+
+# Tuple
+tupla = (1, 2, 3, 4, 5)
+tupla = ('a', 'b', 'c', 'd')
